e0716e829eb0eb084f089c561595aceada955b2b
PelayoCubero <141408440+PelayoCubero@users.noreply.github.com>
2026-06-18 21:11:22 +0200

    Add files via upload

---
+++ Cubix/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+<?php 
+$name = "Cubix";
+include("modulos/head.php"); ?> 
+
+</head>
+<body>
+
+<?php include ("modulos/navbar.php");?>
+
+    <div class ="index">
+  <img  src="imagenes/logo.png" alt="logo">
+
+    </div>
+</body>
+</html>
---
+++ Cubix/modulos/navbar.php
@@ -0,0 +1,23 @@
+  <nav class="navbar navbar-expand-sm bg-bright navbar- barra">
+
+    <a class="nav-link" href="index.html">
+       <img src="imagenes/logo.png" alt="logo"></a>
+     <ul class="navbar-nav">
+   
+           <li class="nav-item">
+         <a class="nav-link" href=" tiendas.html">Tienda</a>
+       </li>
+           <li class="nav-item">
+         <a class="nav-link" href=" algoritmos.html">Tutoriales</a>
+       </li>
+       <li class="nav-item">
+           <a class="nav-link" href="cronometro.html">Cronometro</a>
+         </li>
+
+         <li class="nav-item">
+          <a class="nav-link" href="amigos.html">Amigos</a>
+        </li>
+         
+     </ul>
+   </nav>
+   
