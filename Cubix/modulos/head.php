a6cb7f310c86f2c0175f2b9b13c39d8d9e9ff823
PelayoCubero <141408440+PelayoCubero@users.noreply.github.com>
2026-06-19 03:05:37 +0200

    Add files via upload

--- Cubix/modulos/head.php
+++ Cubix/modulos/head.php
@@ -1,10 +1,6 @@
+
 <link rel="shortcut icon" href="imagenes/logo.png">
 <title><?php echo $name?></title>
-    <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="modulos/style.css">
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<meta charset="utf-8">
+<link rel="stylesheet" href="modulos/style.css">
--- Cubix/modulos/navbar.php
+++ Cubix/modulos/navbar.php
@@ -1,24 +1,11 @@
-  <nav class="navbar navbar-expand-sm bg-bright navbar- barra">
+<nav class="barra">
+  <a class="logo" href="index.php"><img src="imagenes/logo.png"></a>
+  
+  <ul class="menu">
+    <li><a href="tiendas.html">Tienda</a></li>
+    <li><a href="algoritmos.html">Tutoriales</a></li>
+    <li><a href="cronometro.html">Cronometro</a></li>
+    <li><a href="amigos.html">Amigos</a></li>
 
-    <a class="nav-link" href="index.html">
-       <img src="imagenes/logo.png" alt="logo"></a>
-
-     <ul class="navbar-nav">
-   
-           <li class="nav-item">
-         <a class="nav-link" href=" tiendas.html">Tienda</a>
-       </li>
-           <li class="nav-item">
-         <a class="nav-link" href=" algoritmos.html">Tutoriales</a>
-       </li>
-       <li class="nav-item">
-           <a class="nav-link" href="cronometro.html">Cronometro</a>
-         </li>
-
-         <li class="nav-item">
-          <a class="nav-link" href="amigos.html">Amigos</a>
-        </li>
-         
-     </ul>
-   </nav>
-   
+  </ul>
+</nav>
